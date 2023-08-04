4016f4d25d50fe39ed4ab97024a29e24993a64d0
goodjeans <limguanji@gmail.com>
2023-08-04 14:01:51 +0800

    update

---
+++ README.md
@@ -0,0 +1,20 @@
+# Microservice Proof-of-Concept with Android Native and Spring Boot
+
+This repository demonstrates a proof-of-concept (PoC) for communication between an Android Native application and a Spring Boot microservice. The goal is to establish a successful exchange of data between a Spring Boot backend and an Android Native mobile application.
+
+## Technologies used:
+- Android Native
+- Spring Boot
+
+## Requirements:
+- JDK 17
+- Android Studio (Latest Stable Version)
+- Spring Boot (Latest Stable Version)
+- Gradle (Latest Stable Version)
+- Postman (Optional)
+
+## Project Setup:
+
+### Backend Setup:
+Import to eclipse, build jar file
+Run java -jar file.jar
--- src/main/java/com/Restful/microservice/Service.java
+++ src/main/java/com/Restful/microservice/Service.java
@@ -3,8 +3,10 @@ package com.Restful.microservice;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Restful.microservice.model.Courses;
@@ -16,12 +18,12 @@ public class Service {
 		model.addAttribute("message", "Hello");
 		return "Helloworld";
 	}
-	@GetMapping("/ms")
-	public List<Courses> retrieveCourses() {
-		return Arrays.asList(
-				new Courses(1, "Learn AWS", "28minustes")
-				
-				);
+	@Autowired
+	private CurrencyServiceConfiguration configuration;
+	
+	@RequestMapping("/currency-configuration")
+	public CurrencyServiceConfiguration retrieveCourses() {
+		return configuration;
 				
 	}
 	
