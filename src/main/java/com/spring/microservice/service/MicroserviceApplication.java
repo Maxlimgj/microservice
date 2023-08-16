a840cbc190dfd0f2cb403f6bb20e8efa04b82729
goodjeans <limguanji@gmail.com>
2023-08-17 01:55:53 +0800

    change project into gradle instead of using maven

--- src/main/java/com/spring/microservice/service/MicroserviceApplication.java
+++ src/main/java/com/spring/microservice/service/MicroserviceApplication.java
@@ -1,4 +1,4 @@
-package com.Restful.microservice;
+package com.spring.microservice.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/spring/microservice/service/MicroserviceApplicationTests.java
+++ src/test/java/com/spring/microservice/service/MicroserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.Restful.microservice;
+package com.spring.microservice.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
